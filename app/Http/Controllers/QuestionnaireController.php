046bc32823fc9f7ddaf14930ddcbd043eb6efa08
xelarich <xela62400>
2019-03-24 14:13:47 +0100

    Gestion d'erreur

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -11,57 +11,64 @@ use TheSeer\Tokenizer\Exception;
 
 class QuestionnaireController extends Controller
 {
+
+
     public function index()
     {
         return view('questionnaire');
     }
 
+
     public function requete(Request $requete)
     {
+        $tableau = [];
+        $error = false;
         try {
             $connexion = NEW PDO('mysql:host=localhost;dbname=autoevaluationsql', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,));
             $connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        } catch (PDOException $exception) {
-            echo "Erreur : " . $exception;
-        }
-
-        try {
             $sql = $requete->input('requete');
             $query = $connexion->prepare($sql);
-
             $query->execute();
             $resultat = $query->fetchAll();
-
+        } catch (PDOException $e) {
+            $error = true;
+            $tableau[0] = $e->getCode();
+            switch ($e->getCode()) {
+                case 42000:
+                    $tableau[0] = utf8_encode("Erreur Syntaxe de la requête");
+                    break;
+                case '42S22':
+                    $tableau[0] = "Le champ spécifié n'existe pas";
+                    break;
+                case '42S02':
+                    $tableau[0] = "La/Les table(s) spécifié n'existe pas";
+                    break;
+            }
+        }
+        if (!$error) {
             $tableau = [];
             $tableauReponse = [];
             for ($i = 0; $i < sizeof($resultat); $i++) {
                 $donnees = "";
                 foreach ($resultat[$i] as $key => $valeur) {
-                    $donnees .= $key . ' : ' . $valeur . ' ';
+                    $donnees .= $valeur . ' ';
                 }
                 $tableau[$i] = $donnees;
             }
-            $sqlRep = $requete->input('Select * from film');
-            $queryRep = $connexion -prepare($sqlRep);
-            $queryRep->execute();
-            $reponse = $queryRep->fetchAll();
-            for ($i = 0; $i < sizeof($reponse); $i++) {
-                $donnees = "";
-                foreach ($reponse[$i] as $key => $valeur) {
-                    $donnees .= $key . ' : ' . $valeur . ' ';
-                }
-                $tableauReponse[$i] = $donnees;
-            }
-
-
-
-            return view('questionnaire', ['traitement' => $tableau]);
-
         }
-catch (Exception $e)
-{
-return view('questionnaire');
-}
+        /*            $sqlRep = $requete->input('Select * from film');
+                    $queryRep = $connexion -prepare($sqlRep);
+                    $queryRep->execute();
+                    $reponse = $queryRep->fetchAll();
+                    for ($i = 0; $i < sizeof($reponse); $i++) {
+                        $donnees = "";
+                        foreach ($reponse[$i] as $key => $valeur) {
+                            $donnees .= $key . ' : ' . $valeur . ' ';
+                        }
+                        $tableauReponse[$i] = $donnees;
+                    }*/
 
-}
+        return view('questionnaire', ['traitement' => $tableau]);
+
+    }
 }
--- resources/views/questionnaire.blade.php
+++ resources/views/questionnaire.blade.php
@@ -1,37 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Question n°1</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Question n°1</div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <div class="container">
+                                <form method="post" action="{{ url('questionnaire') }}">
+                                    {{ csrf_field() }}
+                                    <div class="row">
+                                        <div class="input-group">
+                                            <input class="form-control" name="requete"
+                                                   placeholder="Entrez votre requête" required>
+                                        </div>
 
-                <div class="card-body">
-                    <div class="form-group">
-                        <div class="container">
-                            <form method="post" action="{{ url('questionnaire') }}">
-                                {{ csrf_field() }}
-                                <div class="row">
-                                    <div class="input-group">
-                                        <input class="form-control" name="requete" placeholder="Entrez votre requête" required>
                                     </div>
-                                </div>
-                                <br>
-                                <div class="row">
-                                    <div class="input-group">
-                                        <input type="submit" class="btn btn-outline-dark" name="Valider" value="Valider">
+                                    <br>
+                                    <div class="row">
+                                        <div class="input-group">
+                                            <input type="submit" class="btn btn-outline-dark" name="Valider"
+                                                   value="Valider">
+                                        </div>
                                     </div>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
 
-
-                    @if(!empty($traitement))
+                @if(!empty($traitement))
                     <div class="card">
                         <div class="card-header">
                             Réponse traitement
@@ -39,15 +40,15 @@
 
                         <div class="card-body">
                             @for($i = 0; $i<count($traitement); $i++)
-                                {{ utf8_encode($traitement[$i]) }} <br><hr>
+                                {{ utf8_encode($traitement[$i]) }}<br>
                             @endfor
-                        @else
-                            Aucune requête mentionnée
+                            @else
+                                Aucune requête mentionnée
+                        </div>
                     </div>
-                    </div>
-                    @endif
+                @endif
 
+            </div>
         </div>
     </div>
-</div>
 @endsection
