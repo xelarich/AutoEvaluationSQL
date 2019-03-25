1af5864cb17b653e7af01492289ce24297e99758
xelarich <xela62400>
2019-03-25 19:27:37 +0100

    Migration table question

---
+++ database/migrations/2019_03_25_173131_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class QuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function(Blueprint $table) {
+            $table->increments('idQ');
+            $table->string('question');
+            $table->boolean('premier');
+            $table->string('reponse');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('question');
+    }
+}
